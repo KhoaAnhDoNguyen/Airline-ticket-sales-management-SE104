5a06082516af38c53ec2eecade4adf662446dc06
huynhan03 <huynhnhandanang@gmail.com>
2023-10-23 15:42:57 +0700

    AControls

---
+++ Airline-ticket-sales-management/Airline-ticket-sales-management/Form1.cs
@@ -0,0 +1,10 @@
+namespace Airline_ticket_sales_management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
